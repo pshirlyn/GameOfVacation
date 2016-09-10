95626dd50101d04ed19b30c2e73bd0d3cdca96f8
Shirlyn Prabahar <pshirlyn@gmail.com>
2016-09-10 11:54:49 -0700

    add template

--- www/js/app.js
+++ www/js/app.js
@@ -49,7 +49,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     }
   })
-
+/*
   .state('tab.chats', {
       url: '/chats',
       views: {
@@ -59,6 +59,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         }
       }
     })
+
     .state('tab.chat-detail', {
       url: '/chats/:chatId',
       views: {
@@ -78,6 +79,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     }
   });
+  */
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
